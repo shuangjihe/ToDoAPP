26a0b3ed4486ddf1c9e8660700117d529844eae9
andy <466788773@qq.com>
2025-01-11 16:28:33 +0800

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,29 +13,33 @@ android {
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        
+        // 添加火山引擎配置
+        buildConfigField("String", "VOLC_APP_ID", "\"${project.findProperty("VOLC_APP_ID") ?: ""}\"")
+        buildConfigField("String", "VOLC_ACCESS_KEY", "\"${project.findProperty("VOLC_ACCESS_KEY") ?: ""}\"")
+        buildConfigField("String", "VOLC_SECRET_KEY", "\"${project.findProperty("VOLC_SECRET_KEY") ?: ""}\"")
     }
 
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
+    
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    
     buildFeatures {
         viewBinding = true
-    }
-    repositories {
-        maven { url 'https://artifact.bytedance.com/repository/releases' }
     }
 }
 
@@ -44,13 +48,8 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")  // 添加这一行
+    implementation("com.volcengine:speech:1.0.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation("com.google.android.material:material:1.11.0")
-    implementation 'com.iflytek.platform:MSC:3.0.0'
-    implementation files('libs/Msc.jar')
-    implementation 'com.baidu.speech:asr:4.16.1'
-    implementation 'com.volcengine.speech:speech:1.0.0'
 }
