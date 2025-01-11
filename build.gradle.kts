efe2655577fe989784a92d1035b2e024eb308a56
andy <466788773@qq.com>
2025-01-11 16:27:17 +0800

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,9 @@
+// Top-level build file
 plugins {
-    id("com.android.application") version "8.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("com.android.application") version "8.1.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.20" apply false
 }
 
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.2.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
-    }
-}
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-        maven { url = uri("https://artifact.bytedance.com/repository/releases") }
-    }
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
