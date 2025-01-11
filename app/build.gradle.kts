c3ef52102d65127952902bb302336219cff901ff
andy <466788773@qq.com>
2025-01-11 16:30:44 +0800

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,7 +49,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("com.volcengine:speech:1.0.0")
+    implementation("com.volcengine:speech-sdk:1.0.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,7 +11,11 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven { url = uri("https://artifact.bytedance.com/repository/releases") }
+        maven { 
+            url = uri("https://artifact.bytedance.com/repository/Volcengine") 
+            // 或者尝试这个地址
+            // url = uri("https://artifact.bytedance.com/repository/maven-public")
+        }
     }
 }
 
