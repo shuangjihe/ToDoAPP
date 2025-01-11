1501a4a6ebc681b296d5e8a46f3729596cd0d1a4
andy <466788773@qq.com>
2025-01-11 16:26:20 +0800

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,21 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
 }
 
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.2.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
+    }
+}
+
 allprojects {
     repositories {
-        maven { url "https://artifact.bytedance.com/repository/releases" }
+        google()
+        mavenCentral()
+        maven { url = uri("https://artifact.bytedance.com/repository/releases") }
     }
 }
